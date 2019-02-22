7ed96db8e792caae2c583cc7a4f0d761fb3f35ae
Bruno Torres <bruno.torres@gm3s.com.mx>
2019-02-22 16:26:48 -0600

    proyecto

---
+++ ProyectoFinal/entrada.c
@@ -0,0 +1,24 @@
+/*Programa de prueba*/principale(){
+	vero x,y;
+	intero i;
+	i=0;
+	x=2.2;
+	y=x - 0;
+	leggi (y);
+	scrivi ("Valor de y es",y);
+	x = 1 + 3;
+	fare{					/*do*/
+		scrivi ("a");
+	} mentre(x < y);		/*while*/
+	
+	seio(a << b){			/*if*/
+		leggi (a);			/*leer*/
+	}
+	altre{ 					/*else*/
+		scrivi ("HOLA")	;	/*es
+		cribir*/
+	}
+}
+
+vero otrafuncion() {}
+/*Fin de programaS*/
